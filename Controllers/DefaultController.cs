3f3cac6e4d4d555d73bd96d587492fe1aabc7da4
Dperez2020 <dawin2020@hotmail.com>
2020-02-19 12:34:48 -0400

    first commit

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PruebasDivWithName.Models;
+
+namespace PruebasDivWithName.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //Se crea un objeto de una clase cualquiera, este caso de tipo persona
+        persona persona = new persona()
+        {
+            nombre = "Dawin"
+        };
+        // GET: Default
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetData()
+        {
+            //Se envia el objeto convertido a Json
+            return Json(persona) ;
+        }
+    }
+}
---
+++ Models/persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebasDivWithName.Models
+{
+    public class persona
+    {
+        public string nombre { get; set; }
+    }
+}
